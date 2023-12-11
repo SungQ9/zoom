35d40efad8b7d7c4e2e2d955caf71e1e40ac3f4b
sungQ <sungkyulee8@gmail.com>
2023-12-11 19:01:55 +0900

    3rd

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,19 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// 서버로부터의 이벤트 처리
+
+socket.addEventListener("open", () => {
+  console.log("서버와 연결되었습니다");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("서버로부터의 새 메세지 :", message.data, " 를 받았습니다");
+});
+
+socket.addEventListener("close", () => {
+  console.log("서버와의 연결이 해제되었습니다");
+});
+
+setTimeout(() => {
+  socket.send("브라우저에서 보냄 : 안녕 !");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -6,6 +8,27 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/")); // 다른 도메인으로 접근시 home으로
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`); // 동일한 포트에서 http 와 ws request 처리 가능
+
+// 같은 서버에서 http,ws 서버 생성
+// http 서버 생성
+const server = http.createServer(app);
+// http 서버 위에 웹소켓 서버 생성
+const wss = new WebSocket.Server({ server });
+
+// socket을 로그에 찍음  = socket = 연결된 사람
+// 연결되면 handleConnection 함수 호출
+wss.on("connection", (socket) => {
+  console.log("브라우저와 연결되었습니다");
+  socket.on("close", () => console.log("브라우저와의 연결이 해제되었습니다"));
+  socket.on("message", (message) => {
+    // 디코딩 작업 ( 한글처리 )
+    const decodedMessage = Buffer.from(message, "binary").toString("utf-8");
+    console.log(decodedMessage);
+  });
+  socket.send("hello");
+});
+
+server.listen(3000, handleListen);
